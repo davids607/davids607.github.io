3268fb6ca18bca7444ed17e2592dc0b6094454fe
davids607 <stephenson.david80@icloud.com>
2025-04-27 20:33:55 -0400

    Add files via upload

---
+++ creating-a-web-page/Developing a Web Page/script.js
@@ -0,0 +1,7 @@
+// Select the button element
+const button = document.querySelector('button');
+
+// Add a click event listener to the button
+button.addEventListener('click', () => {
+    alert('Button was clicked!');
+});
